fbae9fdb35adf6c9e35a0b10fb0602a6be83d621
Collyz <m.mowla03151@gmail.com>
2022-02-14 00:53:49 -0500

    6th Commit

--- src/Tester.java
+++ src/Tester.java
@@ -4,6 +4,7 @@ public class Tester {
         MeanMedianModeStandDev test1 = new MeanMedianModeStandDev();
         PermutationsAndCombinations test2 = new PermutationsAndCombinations(15, 3);
         SetOperationsB test3 = new SetOperationsB();
+        test3.SetUnionB();
 
 
     }
