28b3fae18d6397807be1b2b979b4147355aa59f5
Collyz <m.mowla03151@gmail.com>
2022-02-14 09:55:28 -0500

    7th Commit

--- src/SetOperations.java
+++ src/SetOperations.java
@@ -1,104 +1,51 @@
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class SetOperations {
+    private ArrayList<Integer> setOne = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6));
+    private ArrayList<Integer> setTwo = new ArrayList<Integer>(Arrays.asList(5, 6, 7, 8, 9));
 
-    public SetOperations(int[] array1, int[] array2){
+    public SetOperations(){
+        System.out.println("Set 1 : " + setOne);
+        System.out.println("Set 2 : " + setTwo);
     }
 
-    public void SetUnion(int[] array1, int [] array2){
-        int totalLength = array1.length + array2.length;
-        int[] union = new int[totalLength];
-
-        for(int i = 0; i < array1.length; i ++){
-            union[i] = array1[i];
+    public void setUnion(){
+        ArrayList<Integer> union = new ArrayList<>();
+        union.addAll(setOne);
+        union.addAll(setTwo);
+        for(int i = 0; i < union.size(); i++){
+            for(int j = i + 1; j < union.size(); j++){
+                if(union.get(i) == union.get(j)){
+                    union.remove(j);
+                }
+            }
         }
-        for(int i = 0; i < array2.length; i++){
-            union[i + array1.length] = array2[i];
-        }
-        for(int i = 0; i < union.length; i ++) {
-            System.out.print(union[i] + " ");
-        }
+        System.out.println("Union of the two sets: " + union);
     }
 
-    public void SetIntersect(int[] array1, int[] array2){
-        int count = 0;
-        int totalLength = array1.length + array2.length;
-        int [] holder = new int[totalLength];
-
-        //If they have equal sizes
-        if(array1.length == array2.length){
-            for(int i = 0; i < array1.length; i++){
-                if(array1[i] == array2[i]){
-                    count++;
+    public void setIntersect(){
+        ArrayList<Integer> intersect = new ArrayList<Integer>();
+        for(int i = 0; i < setOne.size(); i++){
+            for(int j = 0; j < setTwo.size(); j++){
+                if(setOne.get(i) == setTwo.get(j)){
+                    intersect.add(setOne.get(i));
                 }
             }
         }
-        //If they have unequal sizes
-        if (array1.length > array2.length) {
-            for(int i = 0; i < array2.length; i++){
-                for(int j = 0; j < array1.length; j++){
-                    if(array2[i] == array1[j]){
-                        count++;
-                        holder[i] = array2[i];
-                    }
-                }
-            }
-        }
-        else if(array1.length < array2.length){
-            for(int i = 0; i < array1.length; i++){
-                for(int j = 0; j < array2.length; j++){
-                    if(array1[i] == array1[j]){
-                        count++;
-                    }
-                }
-            }
-        }
-
-        int[] intersect = new int[count];
-        for(int i = 0; i < holder.length; i++){
-            if(holder[i] != 0){
-                intersect[i] = holder[i];
-            }
-        }
-        System.out.print("The intersection of the two arrays is: ");
-        for(int i = 0; i < intersect.length; i++){
-            System.out.println(intersect[i] + " ");
-        }
-        System.out.println();
+        System.out.println("Intersect of the two sets:" + intersect);
     }
 
-    public void SetComplement(int[] array1, int [] array2){
-        int arrayHolder = Math.max(array1.length, array2.length);
-        int count = 0;
-        int[] holder = new int[arrayHolder];
-        Scanner in = new Scanner(System.in);
-        System.out.print("One array? (Y / N): ");
-        char input = in.next().charAt(0);
-
-        if(input == 'Y'){
-            System.out.println("The complement of the set is (-infinity, infinity)");
-            System.out.print("Excluding: ");
-            for(int i = 0; i < array1.length; i++){
-                System.out.print(array1[i] + " ");
-            }
-        }
-        if(input == 'N'){
-
-            if(array1.length > array2.length){
-
-                for(int i = 0; i < array1.length; i++){
-
-                    for(int j = 0; j < array2.length;j++){
-
-                        if (array1[i] != array2[j]) {
-                            holder[i] = array1[i];
-                        }
-                    }
+    public void setComplement(){
+        ArrayList<Integer> complement = new ArrayList<Integer>();
+        for(int i = 0; i < setOne.size(); i++){
+            for(int j = 0; j < setTwo.size(); j++){
+                if(setOne.get(i) == setTwo.get(j)){
+                    setTwo.remove(j);
                 }
             }
-            for(int i = 0; i < holder.length; i++){
-                System.out.print(holder[i]);
-            }
         }
+        System.out.println("Complement of  " + setTwo);
     }
 
 
--- src/Tester.java
+++ src/Tester.java
@@ -1,10 +1,11 @@
 public class Tester {
     public static void main(String[] args){
 
-        MeanMedianModeStandDev test1 = new MeanMedianModeStandDev();
-        PermutationsAndCombinations test2 = new PermutationsAndCombinations(15, 3);
-        SetOperationsB test3 = new SetOperationsB();
-        test3.SetUnionB();
+//        MeanMedianModeStandDev test1 = new MeanMedianModeStandDev();
+//        PermutationsAndCombinations test2 = new PermutationsAndCombinations(15, 3);
+        SetOperations test3 = new SetOperations();
+        test3.setIntersect();
+        test3.setComplement();
 
 
     }
