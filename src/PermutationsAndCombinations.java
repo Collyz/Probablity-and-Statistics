220787b1f71aea2b2f0a4e37e83182efe28bb764
Collyz <m.mowla03151@gmail.com>
2022-02-13 22:43:31 -0500

    4th push

--- src/PermutationsAndCombinations.java
+++ src/PermutationsAndCombinations.java
@@ -1,26 +1,39 @@
+import java.math.BigInteger;
+
 public class PermutationsAndCombinations {
-    private int totalObjects;
-    private int grouping;
+    private BigInteger totalObjects;
+    private BigInteger grouping;
+    private BigInteger totalMinusGroup;
 
 
-    public PermutationsAndCombinations(int t, int g) {
-        this.totalObjects = t;
-        this.grouping = totalObjects - g;
+    public PermutationsAndCombinations(int allObjects, int groupOfObjects) {
+        this.totalObjects = BigInteger.valueOf(allObjects);
+        this.grouping = BigInteger.valueOf(groupOfObjects);
+        this.totalMinusGroup = totalObjects.subtract(BigInteger.valueOf(groupOfObjects));
     }
 
-    private int factorial(int fact) {
-        int total = 1;
-        int number = fact;
+    public void permutations() {
+        BigInteger total = factorial(totalObjects);
+        BigInteger groups = factorial(totalMinusGroup);
+        BigInteger perm = total.divide(groups);
+        System.out.println("Permutation is: " + perm);
+    }
+
+    public void combinations(){
+        BigInteger total = factorial(totalObjects);
+        BigInteger groups = factorial(totalMinusGroup);
+        BigInteger combination = total.divide((grouping.multiply(groups)));
+        System.out.println("Combination is: " + combination);
+    }
+
+    private BigInteger factorial(BigInteger num) {
+        BigInteger factorial = BigInteger.ONE;
+        int number = num.intValue();
+        BigInteger iterations = BigInteger.ONE;
         for (int i = 1; i <= number; i++) {
-            total = total * i;
+            factorial = factorial.multiply(iterations);
+            iterations = iterations.add(BigInteger.ONE);
         }
-        return total;
-    }
-
-    public void permuations() {
-        int total = factorial(totalObjects);
-        int groups = factorial(grouping);
-        int perm = total/groups;
-        System.out.println(perm);
+        return factorial;
     }
 }
--- src/Tester.java
+++ src/Tester.java
@@ -2,10 +2,5 @@ public class Tester {
     public static void main(String[] args){
 
 
-        PermutationsAndCombinations test1 = new PermutationsAndCombinations(5, 2);
-        test1.permuations();
-        MonteCarloSimulation test2 = new MonteCarloSimulation();
-        test2.gameShow();
-
     }
 }
