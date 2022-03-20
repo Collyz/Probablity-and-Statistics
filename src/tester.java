46fb34ff8f6907189d68255487a6323b02f19514
Mohammed Mowla <74583371+Collyz@users.noreply.github.com>
2022-03-20 14:19:18 -0400

    Delete src directory

--- src/tester.java
+++
@@ -1,9 +0,0 @@
-import java.math.*;
-
-public class tester {
-    public static void main(String[] args){
-
-        TchebysheffTheorem test = new TchebysheffTheorem();
-        System.out.println(test.tchebysheffTheorem(2));
-    }
-}
