a3874166a0b049de1d42094a8204e5c515c78a0d
Mohammed Mowla <74583371+Collyz@users.noreply.github.com>
2022-05-03 13:28:09 -0400

    Add files via upload

---
+++ Project 3/StatsLibrary/DistributionTester.java
@@ -0,0 +1,7 @@
+public class DistributionTester {
+    public static void main(String[] arg){
+
+        UniformDistribution uDistr = new UniformDistribution();
+        uDistr.runAll(0, 30, 25, 30);
+    }
+}
